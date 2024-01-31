334a8bde4743f171bce06cfa92b225f8a084ff62
clarissaftmh <clarissafatimah@gmail.com>
2024-01-31 08:55:19 +0700

    change index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From the other side" }');
+    res.send('{ "response": "Hello From Clae" }');
 });
 
 app.get('/will', function (req, res) {
